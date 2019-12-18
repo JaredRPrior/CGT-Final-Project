45fedf557b6b1605933ec981044cc6ca9dca3306
Jared <jrprior@bowdoin.edu>
2019-12-18 17:04:12 -0500

    Create process_data.py

---
+++ process_data.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+# Jared Prior, Ian Culnane
+# processes data scraped by stockspider.py
+
+def process_line(line):
+	# processes each line in a 
+	# text file formatted: publisher, article, date, stock prices @ date
+	publisher = line[0]
+	time = line[2]
+	stocks = line[3]
+	if stocks[1] != 0:
+		return publisher, time, stocks
+	else:
+		return publisher, time, False
+
+def process_file(file):
+	# processes a file
+	# calculates influence from publisher to stock
+	for line in file:
+		publisher, time, stocks = process_line(line)
+		if stocks == False:
+			continue
+		else:
+			delta = abs(stocks[1] - stocks[0])
+			print(delta)
+
+def main():
