43941b0f7357a92333c8dd8c03be30a709241dbe
Jared <jrprior@bowdoin.edu>
2019-12-18 02:52:50 -0500

    Update stockspider.py

--- stockspider.py
+++ stockspider.py
@@ -1,9 +1,5 @@
-import scrapy
-from scrapy.selector import Selector
-from scrapy.http import HtmlResponse
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import os
@@ -11,17 +7,19 @@ import json
 import urllib
 import htmldate
 import requests
+import yfinance as yf
+from datetime import datetime, timedelta
 
 def url_gen(query, page_number):
     # generates a URL for a Google News query
     url = "https://www.google.com/search?q=" + query + "&source=lnms&tbm=nws&start=" + str(page_number - 1) + "0"
     return url
 
-def get_data(url, browser, page_number, ceiling, data):
+def get_data(url, browser, page_number, ceiling, outfile, stock):
     # retrives data from given URL, recursively crawls entire news query
     # until the crawler has reached the page ceiling, or there are no
     # more pages left to crawl in the results
-
+    print(page_number)
     browser.get(url)
     browser.implicitly_wait(30)
     table = browser.find_element_by_css_selector("tbody")
@@ -35,21 +33,42 @@ def get_data(url, browser, page_number, ceiling, data):
             try:
                 r = requests.get(a.get_attribute('href'), verify=False)
                 date_published = htmldate.find_date(r.text)
-                data.append(publisher.text, title.text, date_published)
+                stock_info = retrieve_stock_info(stock, date_published)
+                file = open(outfile,"a")
+                string = publisher.text + ", " + title.text.replace("\n", "") + ", " + date_published + str(stock_info)
+                file.write(string)
+                file.write("\n")
+                file.close()
             except:
-                print(publisher.text, title.text)
+                continue
     if max_page != None and page_number < ceiling:
-        get_data(max_page[len(max_page) - 1].get_attribute('href'), browser, page_number + 1, ceiling)
+        url = max_page[len(max_page) - 1].get_attribute('href')
+        get_data(url, browser, page_number + 1, ceiling, outfile, stock)
+
+def retrieve_stock_info(stock, date):
+    date2 = increment_date(date)
+    data = yf.download('REKR', date, date2)
+    if len(data['Close']) > 1:
+        return (data['Close'][0], data['Close'][1])
+    else:
+        return (data['Close'][0], 0)
+
+def increment_date(date):
+    # taken and adapted from https://stackoverflow.com/questions/37089765/add-1-day-to-my-date-in-python
+    date2 = datetime.strptime(date, "%Y-%m-%d")
+    date2 = date2 + timedelta(days=1)
+    datetime.strftime(date2, "%Y-%m-%d")
+    date2= str(date2).split(" ")[0]
+    return date2
 
 def main():
     browser = webdriver.Safari()
-    ceiling = 15
-    queries = ["rekr+stock"]
-    data = []
+    ceiling = 1
+    queries = ["REKR"]
     for query in queries:
-        url = url_gen(query, 1)
-        get_data(url, browser, 0, ceiling, data)
-
-
+        url = url_gen(query + "+stock", 1)
+        outfile = query + ".txt"
+        get_data(url, browser, 0, ceiling, outfile, query)
+        print("One query scraped.")
 
 main()
