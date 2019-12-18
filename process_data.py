811bd25420cddba1711e09d49c85b46e503cd92f
Jared <jrprior@bowdoin.edu>
2019-12-18 17:35:58 -0500

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -2,15 +2,23 @@ from datetime import datetime, timedelta
 # Jared Prior, Ian Culnane
 # processes data scraped by stockspider.py
 
+def process_tuple_string(stocks):
+	# processes stock tuple
+	stock_d1 = stocks[0].replace("(", "")
+	stock_d2 = stocks[1].replace(")", "")
+	return float(stock_d1.strip()), float(stock_d2.strip())
+
 def process_line(line):
 	# processes each line in a 
 	# text file formatted: publisher, article, date, stock prices @ date
 	line = line.split(",")
+	length = len(line)
 	publisher = line[0]
-	time = line[2]
-	stocks = line[3]
-	if stocks[1] != 0:
-		return publisher, time, stocks
+	time = line[length - 3]
+	stock_d1, stock_d2 = process_tuple_string(line[length - 2:])
+	print(stock_d1, stock_d2)
+	if stock_d2 != 0:
+		return publisher, time, (stock_d1, stock_d2)
 	else:
 		return publisher, time, False
 
@@ -23,10 +31,10 @@ def process_file(file):
 			continue
 		else:
 			delta = abs(stocks[1] - stocks[0])
-			print(delta)
+			#print(delta)
 
 def main():
-	queries = ["AEG", "POLA", "CSLT", "REFR", "SEAC", "SMSI", "REKR", "ENSV"]
+	queries = ["AEG"]
 	for file in queries:
 		file_name = file + ".txt"
 		f = open(file_name, "r")
